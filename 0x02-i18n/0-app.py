d7ba0f7077c78473ee4fb70c2a3b588901184fad
mmoh-i <49339501+mmoh-i@users.noreply.github.com>
2023-04-10 22:07:33 +0100

    0. Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
-"""importing flask modules to render
-The page.
-"""
-from flask import flask, render_template
+"""Basic Flask app."""
+from flask import Flask, render_template
 
 
-app = (__flask__)
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.route('/')
-def get_index():
-    """The index rendering"""
-    return render_template('0-index.html') 
+def get_index() -> str:
+    """Return index page."""
+    return render_template('0-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
+Give feedback
